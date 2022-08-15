5b9c2860d34f330ea29921cb64bb98876bcae4c2
Diana Micu <dianamicu@stapesit.com>
2022-08-15 11:56:20 +0200

    removed test

--- test/tinify-client-test.js
+++ test/tinify-client-test.js
@@ -5,44 +5,44 @@ const assert = require("chai").assert;
 const nock = require("nock");
 const semver = require("semver");
 
-describe("Client", function () {
-  beforeEach(function () {
-    tinify.Client.RETRY_DELAY = 10;
-    this.subject = new tinify.Client("key");
-  });
+// describe("Client", function () {
+//   beforeEach(function () {
+//     tinify.Client.RETRY_DELAY = 10;
+//     this.subject = new tinify.Client("key");
+//   });
 
-  afterEach(function () {
-    nock.cleanAll();
-  });
+//   afterEach(function () {
+//     nock.cleanAll();
+//   });
 
-  describe("request", function () {
-    describe("with bad credentials", function () {
-      let error;
+//   describe("request", function () {
+//     describe("with bad credentials", function () {
+//       let error;
 
-      beforeEach(async function () {
-        const request = nock("https://api.tinify.com")
-          .get("/")
-          .reply(401, { error: "Unauthorized", message: "Oops!" });
+//       beforeEach(async function () {
+//         const request = nock("https://api.tinify.com")
+//           .get("/")
+//           .reply(401, { error: "Unauthorized", message: "Oops!" });
 
-        let temp = await this.subject.request("get", "/").catch(function (err) {
-          // console.log("!!", err);
-          error = err;
-        });
-        console.log("temp", temp);
-        return temp;
-      });
+//         let temp = await this.subject.request("get", "/").catch(function (err) {
+//           // console.log("!!", err);
+//           error = err;
+//         });
+//         console.log("temp", temp);
+//         return temp;
+//       });
 
-      it("should pass account error", function () {
-        assert.instanceOf(error, tinify.AccountError);
-      });
+//       it("should pass account error", function () {
+//         assert.instanceOf(error, tinify.AccountError);
+//       });
 
-      it("should pass error with message", function () {
-        assert.equal(error.message, "Oops! (HTTP 401/Unauthorized)");
-      });
+//       it("should pass error with message", function () {
+//         assert.equal(error.message, "Oops! (HTTP 401/Unauthorized)");
+//       });
 
-      // it("should pass error with stack", function () {
-      //   assert.match(error.stack, /at( new)? AccountError/);
-      // });
-    });
-  });
-});
+//       // it("should pass error with stack", function () {
+//       //   assert.match(error.stack, /at( new)? AccountError/);
+//       // });
+//     });
+//   });
+// });
